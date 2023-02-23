539de449d17d3923ff7e6397ff4efe1cac9ca54b
Jiajun Han <jiajunhanh@gmail.com>
2023-02-24 02:36:37 +0900

    Complete ph.c

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t table_locks[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -41,6 +42,8 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&table_locks[i]);
+
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,6 +57,8 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&table_locks[i]);
 
 }
 
@@ -116,6 +121,10 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for (int i = 0; i < NBUCKET; ++i) {
+    pthread_mutex_init(&table_locks[i], NULL);
   }
 
   //
